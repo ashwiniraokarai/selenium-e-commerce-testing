ee60f19deb8833ed07478b2ac69cf287bf42593b
Ash <ashwinirao.karai@gmail.com>
2024-02-06 14:24:36 -0700

    Waiting Strategy: Fluently wait via config, for alert message to disappear

--- src/test/java/com/seleniumeasy/demo/acceptancetests/WhenWaitingForElements.java
+++ src/test/java/com/seleniumeasy/demo/acceptancetests/WhenWaitingForElements.java
@@ -1,16 +1,22 @@
 package com.seleniumeasy.demo.acceptancetests;
 
+import com.seleniumeasy.demo.pageobjects.AlertPage;
 import com.seleniumeasy.demo.pageobjects.ModalDialogPage;
 import net.serenitybdd.annotations.Managed;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 
 public class WhenWaitingForElements {
     @Managed(driver = "chrome")
     WebDriver driver;
     private ModalDialogPage modalDialogPage;
+    private AlertPage alertPage;
 
+    /*
+    * Relies Implicit Wait set to 2000ms in Serenity
+    * */
     @Test
     public void waitingForAModalDialog(){
         modalDialogPage.openModalDialogPage();
@@ -19,10 +25,38 @@ public class WhenWaitingForElements {
 
         modalDialogPage.launchModal();
 
+        //*Implicit wait doing its thing here, hidden away from code*/
+
+        //Assertion works because page has had enough time to load the button
         modalDialogPage.saveChangesButton().shouldBeVisible();
 
         modalDialogPage.saveChanges();
 
+        //*The modal closes following which implicit wait doing its thing here, hidden away from code*/
+
+        //Assertion works because page has had enough time to unload the button
         modalDialogPage.saveChangesButton().shouldNotBeVisible();
+    }
+
+    /*
+     * Relies on Fluent Wait configuration
+     * */
+    @Test
+    public void waitingForAnAlertToDisappear(){
+        String expectedAlertMessage = "I'm an autocloseable success message.";
+
+        alertPage.openAlertPage();
+
+        alertPage.openAutoCloseableSuccessAlertBox();
+
+        Assertions.assertThat(alertPage.message())
+                                                        .contains(expectedAlertMessage);
+
+        //wait for five seconds
+        alertPage.waitForMessageToDisappear(expectedAlertMessage);
+
+    /*        You can no longer assert on the text because the text is gone after the box auto-closes'
+        So we employ the element invisibility as a state indicator technique  to validate*/
+        alertPage.autoCloseableAlertBox.shouldNotBeVisible();
     }
 }
---
+++ src/test/java/com/seleniumeasy/demo/pageobjects/AlertPage.java
@@ -0,0 +1,27 @@
+package com.seleniumeasy.demo.pageobjects;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.support.FindBy;
+
+public class AlertPage extends PageObject {
+    @FindBy(css = ".alert-autocloseable-success")
+    public WebElementFacade autoCloseableAlertBox;
+
+    @FindBy(css = "#autoclosable-btn-success")
+    private WebElementFacade autoCloseableAlertTriggerButton;
+    public void openAlertPage(){
+        openUrl("https://demo.seleniumeasy.com/bootstrap-alert-messages-demo.html");
+    }
+
+    public void openAutoCloseableSuccessAlertBox() {
+        autoCloseableAlertTriggerButton.click();
+    }
+    public String message() {
+       return autoCloseableAlertBox.getText();
+    }
+
+    public void waitForMessageToDisappear(String expectedAlertMessage) {
+            waitForTextToDisappear(expectedAlertMessage);
+    }
+}
